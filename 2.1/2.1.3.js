14910f83ebdbef83b23a772d4fb22f843d608cc0
maxim <scilganon@bigmir.net>
2015-11-02 14:56:20 +0000

    add condition examples

--- 2.1/2.1.3.js
+++ 2.1/2.1.3.js
@@ -15,3 +15,23 @@ var a = 5,
 	b = 3;
 	c = b + a,
 	str = tmp = "Hello, world!";
+
+
+//conditions
+var result = true;
+if(result){
+	console.log("positve branch of condition");
+} else {
+	console.log("negative branch of condition");
+}
+
+var country = "Ukraine",
+	capital = null;
+switch(country){
+	case "Ukraine": capital = "Kiev"; break;
+	case "RF": capital = "Moscow"; break;
+	case "GB": capital = "London"; break;
+	default: capital = "unknown";
+}
+console.log(capital + " is capital of " + country);
+// console.log("%s is capital of %s", capital, country);
