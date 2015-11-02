e4fcf8e81c125bccdf9ff0a7d0b6681634ab24bc
maxim <scilganon@bigmir.net>
2015-11-02 14:25:34 +0000

    add example for script attrs

---
+++ 2.1/2.1.3.js
@@ -0,0 +1,17 @@
+// arithmetic
+console.log(2+2, 4/0, 3*0, 0-0);
+//string
+console.log("Hello"+','+"world" + '!');
+//logical
+console.log(true || false, true || false);
+//primary
+console.log((2+2)*2);
+//left-hand-side
+console.log(new Date());
+
+
+//varaibles
+var a = 5,
+	b = 3;
+	c = b + a,
+	str = tmp = "Hello, world!";
