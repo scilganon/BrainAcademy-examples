d478d7d7fa35d46bf18a7d043986ce2baceb399a
maxim <scilganon@bigmir.net>
2016-03-17 20:38:38 +0200

    server for static-pages

--- frameworks/server/index.js
+++ frameworks/server/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.');
+
+http
+    .createServer(function(req, res) {
+        file.serve(req, res);
+    })
+    .listen(8080);
+
+console.log('Server running on port 8080');
