e905de6c5566074d92a2c412144af9014806face
scilganon <scilganon@bigmir.net>
2017-07-14 19:51:35 +0300

    it works!!!

--- javascript/tasks/form/script.js
+++ javascript/tasks/form/script.js
@@ -1,41 +1,67 @@
-function getBtnText(step){
+define([
+  './components/progress/script.js'
+], function(){
+  function getBtnText(step){
     return step === (steps.length -1)
-        ? 'finish'
-        : 'next';
-}
+      ? 'finish'
+      : 'next';
+  }
 
-function form_init(steps){
-    var currentStep = 0;
+  function form_init(steps){
+      var currentStep = 0;
 
-    function renderStep(step){
+      function renderStep(step){
         document.querySelector('#step').src = steps[step];
         document.querySelector('button').innerText = getBtnText(step);
-    }
+      }
 
-    return  {
+      return  {
         //for replacement
         onChange: function(){},
 
         setStep: function(num){
-            renderStep(num);
-            this.onChange(num);
+          renderStep(num);
+          this.onChange(num);
         },
 
         render: function(){
-            var that = this;
+          var that = this;
 
-            document.querySelector('button').addEventListener('click', function(){
-                if(currentStep === (steps.length -1)){
-                    return console.log('finish');
-                }
+          document.querySelector('button').addEventListener('click', function(){
+            if(currentStep === (steps.length -1)){
+              return console.log('finish');
+            }
 
-                currentStep++;
-
-                renderStep(currentStep);
-                that.onChange(currentStep);
-            });
+            currentStep++;
 
             renderStep(currentStep);
+            that.onChange(currentStep);
+          });
+
+          renderStep(currentStep);
         }
-    };
-}
+      };
+    }
+
+  var steps = [
+    "./steps/1.html",
+    "./steps/2.html"
+  ];
+  var progressEl = document.querySelector('.progress');
+
+  var form = form_init(steps);
+  var progress =  progress_init(progressEl, {
+    count: steps.length
+  });
+
+  progress.onChange = function(step){
+    form.setStep(step)
+  };
+
+  form.onChange = function(step){
+    progress.setStep(step);
+  };
+
+  progress.render();
+  form.render();
+});
